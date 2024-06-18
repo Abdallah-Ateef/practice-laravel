81f4dfcdb0c0f6cbef79bccebeae97f680acba0d
Abdallah <abdallahatefelsayed@gmail.com>
2024-06-18 13:31:12 +0300

    make crud operation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>practice laravel</title>
+    <title>Add new post</title>
     <!-- Include boostrap-->
     <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
     <script  src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -13,7 +13,23 @@
 
 </head>
 <body>
-    
+    <div class="createpost">
+        <div class="container">
+            <h3 class="text-center mt-5">Add post</h3>
+            <form class="mt-5" method="POST" action="{{route('post.insert')}}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                <div class="mb-3">
+                  <label for="exampleInputtitle" class="form-label">Title</label>
+                  <input type="text" class="form-control" id="exampleInputtitle" name="title">
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputbody" class="form-label">Body</label>
+                  <input type="text" class="form-control" id="exampleInputbody" name="body">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+        </div>
+    </div>
  
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,9 @@ Route::get('/', function () {
 });
 
 Route::get('/post/create',[PostController::class,'create']);
+Route::get('/posts',[PostController::class,'index'])->name('posts');
+Route::post('/post/insert',[PostController::class,'insert'])->name('post.insert');
+Route::get('post/edit/{id}',[PostController::class,'edit'])->name('post.edit');
+Route::post('/post/update/{id}',[PostController::class,'update'])->name('post.update');
+Route::get('post/delete/{id}',[PostController::class,'delete'])->name('post.delete');
+Route::get('/posts/delete/all',[PostController::class,'deleteAll'])->name('post.delete.all');
